517f2c65d9a2f5b29e0e691632eb91085e237869
Frederik Gladhorn <frederik.gladhorn@remarkable.com>
2020-08-20 16:39:37 +0200

    Fix exports when building as shared lib
    We fake this for xochitl, we still need to do it right.

--- src/karchive_export.h
+++ src/karchive_export.h
@@ -5,7 +5,18 @@
 // to make the karkive module compile without
 // the kdecore headers available.
 
-#define KARCHIVE_EXPORT
 #define KARCHIVE_NO_DEPRECATED
 
+#include <qglobal.h>
+
+// clang-format off
+#ifdef XOCHITL_SHARED
+#  if defined(XOCHITL_BUILD_KARCHIVE)
+#    define KARCHIVE_EXPORT Q_DECL_EXPORT
+#  else
+#    define KARCHIVE_EXPORT Q_DECL_IMPORT
+#  endif
+#else
+#  define KARCHIVE_EXPORT
 #endif
+// clang-format on
